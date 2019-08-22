984f7c91ff46273c71185bf2d2b3dd87905405f4
nate095 <syed.hassan@iskpro.com>
2019-08-22 16:05:33 +0530

    remove clean url for css

--- wp-content/mu-plugins/Themeoptions.php
+++ wp-content/mu-plugins/Themeoptions.php
@@ -70,7 +70,6 @@ function _remove_script_version( $src ){
 	return $parts[0];
 }
 add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
-add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
 /*End Remove Script Version*/
 
 
